f3335f79775be5d0d7cd329691fd6830073dc37a
Honey2339 <prasoon2study@gmail.com>
2024-01-15 16:53:33 +0530

    Fixed Navbar Render

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Provider session={session}>
           <main>
+            <NavBar />
             <div id="bg-grid">
               <div id="blur-grid"></div>
             </div>
